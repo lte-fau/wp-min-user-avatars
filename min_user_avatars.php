7fac02cea69531cb4fb074350ab39ef8169521f3
Stefan Lindner <stefan.lindner@fau.de>
2015-05-15 13:30:32 +0200

    better scaling, option to delete scaled pictures

--- min_user_avatars.php
+++ min_user_avatars.php
@@ -61,7 +61,7 @@ class min_user_avatars {
 								);
 		//save upload_path
 		add_filter( 'upload_dir',  array( $this, 'set_avatar_directory') );
-    self::$options['upload_path']=wp_upload_dir();
+		self::$options['upload_path']=wp_upload_dir();
     remove_filter( 'upload_dir',  array( $this, 'set_avatar_directory') );
 		// Text domain
 		$this->load_textdomain();
@@ -74,7 +74,7 @@ class min_user_avatars {
 		add_action( 'edit_user_profile_update',  array( $this, 'edit_user_profile_update' )        );//others profile
 
 		// Filters
-		add_filter( 'get_avatar',                array( $this, 'get_avatar_filter'               ), 20, 5 );
+		add_filter( 'get_avatar',                array( $this, 'get_avatar_filter'               ), 20 ,6);
 		add_filter( 'avatar_defaults',           array( $this, 'avatar_defaults_filter'          )        );
 	}
 
@@ -98,7 +98,9 @@ class min_user_avatars {
 	public function admin_init() {
 		// Register/add the Discussion setting to restrict avatar upload capabilites
 		register_setting( 'discussion', 'min_user_avatars_caps', array( __CLASS__, 'sanitize_options' ) );
+		register_setting( 'discussion', 'min_user_delete_non_scaled', array( __CLASS__, 'sanitize_options' ) );
 		add_settings_field( 'min-user-avatars-caps', __( 'Local Avatar Permissions', 'min-user-avatars' ), array( __CLASS__, 'avatar_settings_field' ), 'discussion', 'avatars' );
+
 	}
 
 
@@ -116,8 +118,15 @@ class min_user_avatars {
 					<?php checked(self::$options['deny_upload'], 1 ); ?>/>
 			<?php _e( 'Only allow users with file upload capabilities to upload local avatars (Authors and above)', 'min-user-avatars' ); ?>
 		</label>
+		<br>
+		<label for="min_user_delete_non_scaled">
+			<input type="checkbox" name="min_user_delete_non_scaled" id="min_user_delete_non_scaled" value="1"/>
+			<?php _e( 'Check and save to delete all scaled picture(will be auto-regenerated at request).', 'min-user-avatars' ); ?>
+		</label>
 		<?php
 	}
+	
+	
 
 	/**
 	 * Sanitize the Discussion settings option
@@ -128,6 +137,15 @@ class min_user_avatars {
 	 */
 	public function sanitize_options( $input ) {
 		$new_input['min_user_avatars_caps'] =  $input['min_user_avatars_caps'] ? 1 : 0;
+		print_r($input);
+		
+		if($input['min_user_delete_non_scaled']){
+		echo "delete requested";
+		self::min_user_delete_scaled();
+		echo "All deleted";
+		
+		break;
+		}
 		return $new_input;
 	}
 
@@ -142,9 +160,9 @@ class min_user_avatars {
 	 * @param boolean $alt 
 	 * @return string
 	 */
-	public function get_avatar_filter( $avatar = '', $id_or_email, $size = 96, $default = '', $alt = false ) {
-
-		// Determine if we recive an ID or string
+	public function get_avatar_filter( $avatar = '', $id_or_email, $size = 96, $default = '', $alt = false,$args=null ) {
+  if ($default==="NOLINK"){$avatar="";}
+			// Determine if we recive an ID or string
 		if ( is_numeric( $id_or_email ) )
 			$user_id = (int) $id_or_email;
 		elseif ( is_string( $id_or_email ) && ( $user = get_user_by( 'email', $id_or_email ) ) )
@@ -156,41 +174,76 @@ class min_user_avatars {
 			return $avatar;
 
 		$local_avatars = get_user_meta( $user_id, 'min_user_avatar', true );
-		/*print("<pre>");
+	/*	print("<pre>");
 		print_r($local_avatars);
 		print("</pre>");*/
 		if ( empty( $local_avatars ) || empty( $local_avatars['full'] ) )
-			return $avatar;
+    {
+    return $avatar;
+    }
 
-		$size = (int) $size;
+      $defaults = array(
+            // get_avatar_data() args.
+            'size'          => 96,
+            'height'        => null,
+            'width'         => null,
+            'default'       => get_option( 'avatar_default', 'mystery' ),
+            'force_default' => false,
+            'rating'        => get_option( 'avatar_rating' ),
+            'scheme'        => null,
+            'alt'           => '',
+            'class'         => null,
+            'force_display' => false,
+            'extra_attr'    => '',
+	       );
 
+      if(empty($args)){$args = array();}
+          $args['size']    = (int) $size;
+	        $args['default'] = $default;
+	        $args['alt']     = $alt;
+     $args = wp_parse_args( $args, $defaults );
+	
+	        if (empty( $args['height'])) {
+	                $args['height'] = $args['size'];
+	        }
+	        if ( empty( $args['width'] ) ) {
+	                $args['width'] = $args['size'];
+	        }
+
+
+		$sizewidth = (int) $args['width'];
+		$sizeheight = (int) $args['height'];
+		$sizename=$sizewidth.'x'.$sizeheight;
 		if ( empty( $alt ) )
-			$alt = get_the_author_meta( 'display_name', $user_id );
+			{$alt = get_the_author_meta( 'display_name', $user_id );}
 
 		// Generate a new size
-		if ( empty( $local_avatars[$size] ) ) {
+		if (empty( $local_avatars[$sizename] ) ) {
 
 			$avatar_full_path = str_replace( self::$options['upload_path']['baseurl'], self::$options['upload_path']['basedir'], $local_avatars['full'] );
-			$image            = wp_get_image_editor( $avatar_full_path );
+
+			$image= wp_get_image_editor( $avatar_full_path );
 
 			if ( ! is_wp_error( $image ) ) {
-				$image->resize( $size, $size, true );
+				$image->resize( $sizewidth, $sizeheight, true );
 				$image_sized = $image->save();
-			}
+			}else
+			{echo "error at resize";}
 
 			// Deal with original being >= to original image (or lack of sizing ability)
-			$local_avatars[$size] = is_wp_error( $image_sized ) ? $local_avatars[$size] = $local_avatars['full'] : str_replace( self::$options['upload_path']['basedir'], self::$options['upload_path']['baseurl'], $image_sized['path'] );
+			$local_avatars[$sizename] = is_wp_error( $image_sized ) ? $local_avatars[$size] = $local_avatars['full'] : str_replace( self::$options['upload_path']['basedir'], self::$options['upload_path']['baseurl'], $image_sized['path'] );
 
 			// Save updated avatar sizes
 			update_user_meta( $user_id, 'min_user_avatar', $local_avatars );
 
-		} elseif ( substr( $local_avatars[$size], 0, 4 ) != 'http' ) {
-			$local_avatars[$size] = home_url( $local_avatars[$size] );
+		} elseif ( substr( $local_avatars[$sizename], 0, 4 ) != 'http' ) {
+			$local_avatars[$sizename] = home_url( $local_avatars[$sizename] );
 		}
+		
 
 		$author_class = is_author( $user_id ) ? ' current-author' : '' ;
-		$avatar       = "<img alt='" . esc_attr( $alt ) . "' src='" . $local_avatars[$size] . "' class='avatar avatar-{$size}{$author_class} photo' height='{$size}' width='{$size}' />";
-		return apply_filters( 'min_user_avatar', $avatar );
+		$avatar       = "<img alt='" . esc_attr( $alt ) . "' src='" . $local_avatars[$sizename] . "' class='avatar avatar-{$sizename}{$author_class} photo' height='{$sizeheight}' width='{$sizewidth}' />";
+		return $avatar;
 }
 	
 
@@ -309,7 +362,7 @@ class min_user_avatars {
 	 * @return array
 	 */
 	public function avatar_defaults_filter( $avatar_defaults ) {
-		remove_action( 'get_avatar', array( $this, 'get_avatar' ) );
+		remove_action( 'get_avatar', array( $this, 'get_avatar_filter' ) );
 		return $avatar_defaults;
 	}
 
@@ -333,7 +386,7 @@ class min_user_avatars {
 	}
 	
 	/**
-	 * Set upload dir
+	 * Override upload dir
 	 *
 	 * @since 1.0.0
 	 * @param int $user_id
@@ -356,17 +409,55 @@ class min_user_avatars {
 	 * @return string
 	 */
 	public function unique_filename_callback( $dir, $name, $ext ) {
+    $ext=strtolower($ext);//only lowercase
+		if(($ext===".jpeg")||($ext===".jpe")){$ext=".jpg";}//only use .jpg
 		$user = get_user_by( 'id', (int) $this->user_id_being_edited );
-		$name = $base_name = sanitize_file_name( $user->display_name . '' );
+		$name = $basename=sanitize_file_name( $user->user_nicename . '' );
 		$number = 1;
 
-		while ( file_exists( $dir . "/$name$ext" ) ) {
-			$name = $base_name . '_' . $number;
+		while ( file_exists( $dir . "/" . $name . $ext) ) {
+			$name = $basename . '_' . $number;
 			$number++;
 		}
-
+		if (empty($name))
+		  {
+		    $name=$this->user_id_being_edited;
+		  }
 		return $name . $ext;
 	}
+	/**
+	 * Delete only scaled versions
+	 *
+	 */
+	public function min_user_delete_scaled() {
+	$users = get_users_of_blog();
+	echo "delete all scaled pictures";
+
+	foreach ( $users as $user ){
+	echo "<br>#########NEW User: ".$user->display_name." ###########<br>";
+    $avatars=get_user_meta( $user->ID, 'min_user_avatar', true );
+    if(!is_array($avatars))
+      {
+      echo "No Avatar found!";
+      continue;
+      }
+   foreach ($avatars as $size=>$url)
+    {//echo $size;
+      if (strtolower($size)==="full")//dont delete unscaled picture
+        {
+         echo "Skipping unscaled version<br>";
+        continue;
+        }
+    	$old_avatar_path = str_replace( self::$options['upload_path']['baseurl'], self::$options['upload_path']['basedir'], $url );
+      echo "Deleting size ".$size." and file: ".$old_avatar_path."<br>";
+			unset($avatars[$size]);
+			@unlink( $old_avatar_path );
+    }
+    update_user_meta( $user->ID, 'min_user_avatar', $avatars);
+  }
+}
+	
+	
 }
 $min_user_avatars = new min_user_avatars;
 
@@ -382,6 +473,7 @@ function min_user_avatars_uninstall() {
 	foreach ( $users as $user )
 		$min_user_avatars->avatar_delete( $user->user_id );
 
-	delete_option( 'min_user_avatars_caps' );
+	delete_option( 'min_user_avatars_caps');
 }
+
 register_uninstall_hook( __FILE__, 'min_user_avatars_uninstall' );
